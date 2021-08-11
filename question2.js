198bec015916138e5fccc3b61293ee5f3a9f8e9f
saweyyan <87162289+saweyyan@users.noreply.github.com>
2021-08-11 10:32:27 +0300

    Create question2.js

---
+++ question2.js
@@ -0,0 +1,19 @@
+
+/*
+  number less than 10 return => "00" + number
+  number more than 10 and < 100 return => "0" + number
+  number more than or equal 100 return =>  number
+*/
+function test(number){
+    // write if statement here
+    
+    // return the result 
+}
+
+
+// test cases ( don't change these 4 lines )
+console.log(test(9)) // should return 9
+console.log(test(20)) // should return "020" 
+console.log(test(110))  // should return "110" 
+console.log(test(10))  // undefined
+// you can add more test cases if you want 
