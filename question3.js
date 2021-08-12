f0c1a5b90612e204f0748cc49021de963da90b5c
Omar <omar.ajaj@saweyyan.com>
2021-08-13 00:22:32 +0300

    Solved Question 3

--- question3.js
+++ question3.js
@@ -1,8 +1,8 @@
 // update the values so that all condition will log true into the console
 
-var num1 = 10;
-var num2 = 10;
-var num3 = "10";
+var num1 = 2;
+var num2 = 1;
+var num3 = +"2";
 var num4 = 20;
 
 /*
