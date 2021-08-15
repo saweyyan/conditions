fc330e7e2dbe7f8189934f2593d9a509ca515edf
huthaifaTayeh <huthaifazmi@gmail.com>
2021-08-15 22:50:31 +0300

    Answered Questions1 correctly

--- question1.js
+++ question1.js
@@ -2,11 +2,11 @@
 
 
 // A. replace ? with operator to output true 
-console.log(100 ? "100"); // true
-console.log(100 ? 1000); // true
-console.log(110 ? 100 ? 10 ? 20); // true
-console.log(-10 ? "-10"); // true
-console.log(? 20 ? false); // true
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110!== 100 > 10 > 20); // true
+console.log(-10 == "-10"); // true
+console.log(! 20 == false); // true
             
             
             
@@ -21,14 +21,14 @@ let num2 = 20;
   - YOU CAN'T USE THE SAME VARIABLE MORE THAN ONCE IN THE SAME SOULTION LIKE THIS ( num1 == num1 )
 */
 // example
-console.log(num1 < num2. ); // true
-console.log(num1 <= num2. ); // true
+console.log(num1 < num2 ); // true
+console.log(num1 <= num2 ); // true
 console.log(num2 > num1 ); // true
 console.log(num2 >= num1 ); // true
 // minmmum 3 solution
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
-console.log("Your Code Here"); // true
+console.log(num1 != num2); // true
+console.log(! (num1 == num2)); // true
+console.log(! (num1> num2)); // true
 // you can add more solutions here 
 // console.log(); // true
 
@@ -41,6 +41,6 @@ let a = 20;
 let b = 30;
 let c = 10;
 
-console.log(a ? b && a ? c || a ? b); // true
-console.log(a ? b ? a ? c); // true
-console.log(?(a ? b) && !(a ? b) && ?(a ? c) && ?(a ? c)); // true
+console.log(a < b && a > c || a == b); // true
+console.log(a < b || a > c); // true
+console.log(!(a > b) && !(a > b) && !(a == c) && !(a == c)); // true
